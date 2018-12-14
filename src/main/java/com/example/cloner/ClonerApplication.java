41db156595df3ac43b8b328c0ad4c1dd0232c743
Jose <joseenrique86@gmail.com>
2018-12-14 12:29:43 +0000

    Added init files

---
+++ src/main/java/com/example/cloner/ClonerApplication.java
@@ -0,0 +1,14 @@
+package com.example.cloner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClonerApplication.class, args);
+	}
+
+}
+
